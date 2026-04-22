4038759e41eca0269761d317eea02250645afd9d
Pedro Lucas <143295682+plpedreira@users.noreply.github.com>
2026-04-22 12:03:32 -0300

    feat: React study (components, props and CSS Modules)

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,10 +1,14 @@
+import "./global.css"
+
 import { Button } from "./components/button"
+
+import styles from "./app.module.css"
 
 export function App() {
     return( 
-     <div>
-     <Button name="Criar" onClick={() => alert("Criar")}/>
-     <Button name="Editar"/>
+     <div className = {styles.container}>
+     <Button name="Adicionar"/>
+     <span>0</span>
      <Button name="Remover"/>
      </div>)
 }
